078991c94c26feef45d382bad4064a491b26c69d
Joshua Jainga <jjainga@gmail.com>
2020-09-11 07:19:18 -0700

    Added Validations and Arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,14 @@
+// //Asignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// //Write password to the #password input
+// function writepassword() {
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+
+//     passwordText.value = passwaord;
+// }
+
 //Prompting the user if they want a password
 var start = confirm("Do you want to generate a password?")
 
@@ -10,13 +21,34 @@ var number = confirm("Do you want to use numbers in your password?")
 var symb = confirm("Do you want to use symbols in your password?")
 var length = prompt("How many characters would you like between 8 and 128?")
 
-var passwordlength = Number(length)
+var passwordLength = Number(length)
 
-console.log(passwordlength)
+//Validation for password criteria
+if (lower === false &&
+    upper === false && 
+    number === false && 
+    symb === false) {
+        alert("Please select at least one criteria to generate your password.")
+    }
 
-//Conditions to use the functions
+//Validation for length of password
+if (passwordLength < 8 ||
+    passwordLength > 128) {
+        alert("Please select a legnth between 8 and 128, thank you.")
+    }
 
-var passwordfunctions = {
+console.log(passwordLength)
+// Create array for combining all the chosen characters
+var passwordHolder =[]
+//For loop the amount of times the user put in
+for(var i =0; i<passwordLength;i++) {
+
+}
+
+//Array of the methods fromt he object passwordfunctions
+var passwordFunc = [passwordFunctions.lowerCase(), passwordFunctions.upperCase(), passwordFunctions.numbers(), passwordFunctions.symbols()]
+
+var passwordFunctions = {
 //Functions for generating random characters
 lowerCase: function randomLowCase() {
  return String.fromCharCode(Math.floor(Math.random()*26)+97);
@@ -35,21 +67,27 @@ symbols: function randomSymbol() {
     return symbol[Math.floor(Math.random()* symbol.length)];
     
 }}
+
+//TODO: Add validations to make sure atleast one type of char is selected and alert user if no types are selected
+
+//TODO: After I m done with the prompts a password is  generated
+
+
 //Console Logs to check functions
 
-console.log(passwordfunctions.lowerCase())
+console.log(passwordFunctions.lowerCase())
 
 console.log("---------------------")
 
-console.log(passwordfunctions.upperCase())
+console.log(passwordFunctions.upperCase())
 
 console.log("---------------------")
 
-console.log(passwordfunctions.numbers())
+console.log(passwordFunctions.numbers())
 
 console.log("---------------------")
 
-console.log(passwordfunctions.symbols())
+console.log(passwordFunctions.symbols())
 
 
 
