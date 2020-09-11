bfa9a198c97a75fe91590942d1ce1ebf8a194a3b
Joshua Jainga <jjainga@gmail.com>
2020-09-11 16:48:34 -0700

    Created Working Password Generator

--- Develop/script.js
+++ Develop/script.js
@@ -21,7 +21,7 @@ var number = confirm("Do you want to use numbers in your password?")
 var symb = confirm("Do you want to use symbols in your password?")
 var length = prompt("How many characters would you like between 8 and 128?")
 
-var passwordLength = Number(length)
+var passwordLength = parseInt(length)
 
 //Validation for password criteria
 if (lower === false &&
@@ -36,59 +36,74 @@ if (passwordLength < 8 ||
     passwordLength > 128) {
         alert("Please select a legnth between 8 and 128, thank you.")
     }
-
-var criteriaArr = [{lower}, {upper}, {number}, {symb}].filter (
-item => Object.values(item)[0]
-);
-console.log(criteriaArr)
-
 console.log(passwordLength)
-// Create array for combining all the chosen characters
-var passwordHolder =[]
-//For loop the amount of times the user put in
-for(var i =0; i<passwordLength;i++) {
 
+//Functions for generating random characters
+lowerCase = function randomLowCase() {
+    return String.fromCharCode(Math.floor(Math.random()*26)+97);
+   }
+upperCase = function randomUpperCase() {
+       return String.fromCharCode(Math.floor(Math.random()*26)+65);   
+   }
+numbers = function randomNumber() {
+       return String.fromCharCode(Math.floor(Math.random()*10)+48);   
+   }    
+symbols = function randomSymbol() {
+       const symbol = ["!", "@", "#", "$", "%", "^", "&", "<", ">", "{", "}", "/", "?", "=", "+", "-", "_"]
+       return symbol[Math.floor(Math.random()* symbol.length)];
+   }
+var randomCharacters = [];
+//For loop to pull random charater from the passwordfunciton Array
+for (var i=0; i < passwordLength; i++) {
+//Array containing functions to generate random characters, function only pushed to array if the user selected true for confirm
+    var passwordFunctions = [];
+    if (lower === true) {
+    passwordFunctions.push(lowerCase());
+    }
+    if (upper === true) {
+    passwordFunctions.push(upperCase());
+    }
+    if (number === true) {
+    passwordFunctions.push(numbers());
+    }
+    if (symb === true) {
+    passwordFunctions.push(symbols());
+    }
+    //Check if Array catches functions
+    console.log(passwordFunctions)
+    var letter = passwordFunctions[Math.floor(Math.random()*passwordFunctions.length)];
+    console.log(letter);
+    randomCharacters.push(letter)
 }
+//checking if finalpassword recieved values
+console.log(randomCharacters)
 
-//Object containing functions to generate random characters
-var passwordFunctions = {
-    //Functions for generating random characters
-    lowerCase: function randomLowCase() {
-     return String.fromCharCode(Math.floor(Math.random()*26)+97);
-    },
-    
-    upperCase: function randomUpperCase() {
-        return String.fromCharCode(Math.floor(Math.random()*26)+65);   
-    },
-    
-    numbers: function randomNumber() {
-        return String.fromCharCode(Math.floor(Math.random()*10)+48);   
-    },
-    
-    symbols: function randomSymbol() {
-        const symbol = ["!", "@", "#", "$", "%", "^", "&", "<", ">", "{", "}", "/", "?", "=", "+", "-", "_"]
-        return symbol[Math.floor(Math.random()* symbol.length)];
-        
-    }}
-//Array of the methods fromt he object passwordfunctions
-var passwordFunc = [passwordFunctions.lowerCase(), passwordFunctions.upperCase(), passwordFunctions.numbers(), passwordFunctions.symbols()]
+//Making final password
+var finalpassword = randomCharacters.join("");
+console.log(finalpassword)
+
+
+console.log("---------------------")
+//Variable to pull a character out of the Array
+var passwordCharacter = passwordFunctions[0];
+console.log(passwordCharacter)
 
 // //Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
 
 //Console Logs to check functions
-
-console.log(passwordFunctions.lowerCase())
+console.log("---------------------")
+console.log(lowerCase())
 
 console.log("---------------------")
 
-console.log(passwordFunctions.upperCase())
+console.log(upperCase())
 
 console.log("---------------------")
 
-console.log(passwordFunctions.numbers())
+console.log(numbers())
 
 console.log("---------------------")
 
-console.log(passwordFunctions.symbols())
+console.log(symbols())
