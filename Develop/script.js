e678fcb8e7ccc21ae302ea53e8e9ba0982ba05bc
Joshua Jainga <jjainga@gmail.com>
2020-09-11 20:15:14 -0700

    Password Generator Works

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,43 @@
-//Asignment Code
-var generateBtn = document.querySelector("#generate");
+//Variables to link javascript functions with id's
+var generatebtn = document.querySelector("#generate");
+var randomPassword = document.querySelector("#password");
 
-//Write password to the #password input
-function writepassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = passwaord;
-}
-
+//Click generate to start running the code
+generatebtn.addEventListener("click", writePassword = function() {
 //Prompting the user if they want a password
 var start = confirm("Do you want to generate a password?")
-
-
-
 //Prompts for user to input what they want for 
 var lower = confirm("Do you want to use lower case letters in your password?")
 var upper = confirm("Do you want to use upper case letters in your password?")
 var number = confirm("Do you want to use numbers in your password?")
 var symb = confirm("Do you want to use symbols in your password?")
 var length = prompt("How many characters would you like between 8 and 128?")
-
+//making length an integer
 var passwordLength = parseInt(length)
+console.log(passwordLength)
 
-//Validation for password criteria
+//Validating password criteria
 if (lower === false &&
     upper === false && 
     number === false && 
-    symb === false) {
-        alert("Please select at least one criteria to generate your password.")
+    symb === false ||
+    passwordLength < 8 ||
+    passwordLength > 128 ||
+    ) {
+        alert("Please select at least one criteria and make sure the password length is between 8 and 128 characters to generate your password.")
     }
-
-//Validation for length of password
-if (passwordLength < 8 ||
-    passwordLength > 128) {
-        alert("Please select a legnth between 8 and 128, thank you.")
-    }
-console.log(passwordLength)
-
+else {
 //Functions for generating random characters
-lowerCase = function randomLowCase() {
-    return String.fromCharCode(Math.floor(Math.random()*26)+97);
+var lowerCase = function randomLowCase() {
+        return String.fromCharCode(Math.floor(Math.random()*26)+97);
    }
-upperCase = function randomUpperCase() {
+var upperCase = function randomUpperCase() {
        return String.fromCharCode(Math.floor(Math.random()*26)+65);   
    }
-numbers = function randomNumber() {
+var numbers = function randomNumber() {
        return String.fromCharCode(Math.floor(Math.random()*10)+48);   
    }    
-symbols = function randomSymbol() {
+var symbols = function randomSymbol() {
        const symbol = ["!", "@", "#", "$", "%", "^", "&", "<", ">", "{", "}", "/", "?", "=", "+", "-", "_"]
        return symbol[Math.floor(Math.random()* symbol.length)];
    }
@@ -70,40 +59,39 @@ for (var i=0; i < passwordLength; i++) {
     passwordFunctions.push(symbols());
     }
     //Check if Array catches functions
-    console.log(passwordFunctions)
+    console.log(passwordFunctions);
     var letter = passwordFunctions[Math.floor(Math.random()*passwordFunctions.length)];
     console.log(letter);
-    randomCharacters.push(letter)
+    randomCharacters.push(letter);
 }
 //checking if finalpassword recieved values
-console.log(randomCharacters)
+console.log(randomCharacters);
 
 //Making final password
 var finalpassword = randomCharacters.join("");
-console.log(finalpassword)
+console.log(finalpassword);
+//Putting finalpassword into the input
+randomPassword.textContent = finalpassword;
+}})
+
+// console.log("---------------------")
+// //Variable to pull a character out of the Array
+// var passwordCharacter = passwordFunctions[0];
+// console.log(passwordCharacter)
 
 
-console.log("---------------------")
-//Variable to pull a character out of the Array
-var passwordCharacter = passwordFunctions[0];
-console.log(passwordCharacter)
+// //Console Logs to check functions
+// console.log("---------------------")
+// console.log(lowerCase())
 
-// //Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// console.log("---------------------")
 
+// console.log(upperCase())
 
-//Console Logs to check functions
-console.log("---------------------")
-console.log(lowerCase())
+// console.log("---------------------")
 
-console.log("---------------------")
+// console.log(numbers())
 
-console.log(upperCase())
+// console.log("---------------------")
 
-console.log("---------------------")
-
-console.log(numbers())
-
-console.log("---------------------")
-
-console.log(symbols())
+// console.log(symbols())
